8bf3c90af0ee350e82a3aacd7b379fdaf0f60712
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2020-12-29 21:44:20 +0000

    deserialize

--- serialkiller/Program.cs
+++ serialkiller/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Text;
@@ -10,37 +11,72 @@ namespace serialkiller
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {
             Console.WriteLine("Hello World!");
+
+
             Cocktail cocktailTim = new();
             cocktailTim.Id = 1;
-            cocktailTim.Name = "Roberts";
+            cocktailTim.Name = "Martini";
             cocktailTim.Price = 15.25;
             cocktailTim.Rating = 4.8;
-            
-            WriteClasstoFile(cocktailTim);
+
+
+            List<Cocktail> cocktailList = new();
+            cocktailList.Add(new Cocktail() { Id = 1, Name = "Martini", Price = 15.25, Rating = 4.8 });
+            cocktailList.Add(new Cocktail() { Id = 2, Name = "Manhattan", Price = 15.50, Rating = 4.5 });
+            cocktailList.Add(new Cocktail() { Id = 3, Name = "Domestic Beer", Price = 8.50, Rating = 3.0 });
+            cocktailList.Add(new Cocktail() { Id = 4, Name = "Wine", Price = 10.00, Rating = 3.5 });
+            WriteClasstoFile(cocktailList);
+
+            List<Cocktail> cocktailListFromDisk = new();
+            LoadClassFromFile(cocktailListFromDisk);
+
 
         } // end of main method
 
-        
-
-        public static void WriteClasstoFile(Cocktail _cocktail)
+        public static void LoadClassFromFile(List<Cocktail> _cocktail)
         {
             
+            
+            // read file
+            string dirName = "./Data";
+            string fileName = dirName + "/APIData.txt";
+            string jsonString = File.ReadAllText(fileName);             
+
+            // deserialize into class array
+            var options = new JsonSerializerOptions { WriteIndented = true, };
+
+            _cocktail  = JsonSerializer.Deserialize<List<Cocktail>>(jsonString);
+
+            // pass list back to caller
+
+            
+
+        }
+
+        public static void WriteClasstoFile(List<Cocktail> _cocktail)
+        {
+
+            var options = new JsonSerializerOptions { WriteIndented = true, };
+
+            //string jsonString = JsonSerializer.Serialize(_cocktail, options);
             string jsonString = JsonSerializer.Serialize(_cocktail);
-            jsonString = jsonString + "\n";
-            string dirName =  "./Data";
-            string fileName = dirName + "/APIData.txt";            
+
+            string dirName = "./Data";
+            string fileName = dirName + "/APIData.txt";
 
             // Create Directory creates dir if it does not exists, otherwise it does nothing
-            Directory.CreateDirectory(dirName); 
-            
-            using FileStream APIDataFileStream = File.Open(fileName,FileMode.Append);
+            Directory.CreateDirectory(dirName);
+
+            using FileStream APIDataFileStream = File.Open(fileName, FileMode.Append);
 
             byte[] info = new UTF8Encoding(true).GetBytes(jsonString);
             APIDataFileStream.Write(info, 0, info.Length);
 
         }
+
+
 
 
 
